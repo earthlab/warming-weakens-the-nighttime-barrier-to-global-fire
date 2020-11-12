526d1a964944294223ff71c169cb0563f3d9744e
Michael Koontz <mikoontz@gmail.com>
2020-11-12 12:51:02 -0700

    initial commit of mjk contributions (minus overpass correction)

---
+++ 01_get-goes16/get-af-data_aws.R
@@ -0,0 +1,213 @@
+library(tidyverse)
+library(glue)
+library(terra)
+library(sf)
+library(slider)
+library(purrr)
+library(furrr)
+library(future)
+# library(foreach)
+# library(doParallel)
+
+get_latest_goes <- TRUE
+
+if(get_latest_goes | !file.exists("data/data_output/goes16-filenames.csv")) {
+  source("R/get-af-metadata.R")
+}  
+
+# Read in the GOES metadata acquired from Amazon Earth using get-af-metadata.R script
+goes_af <- readr::read_csv(file = "data/data_output/goes16-filenames.csv", col_types = "ciiiiinicTTTccccc")
+
+# Get the flag values that are important using an example .nc file if not done already
+# flag_vals                            flag_meanings
+# 10                                   good_fire_pixel
+# 11                              saturated_fire_pixel
+# 12                     cloud_contaminated_fire_pixel
+# 13                       high_probability_fire_pixel
+# 14                     medium_probability_fire_pixel
+# 15                        low_probability_fire_pixel
+# 30               temporally_filtered_good_fire_pixel
+# 31          temporally_filtered_saturated_fire_pixel
+# 32 temporally_filtered_cloud_contaminated_fire_pixel
+# 33   temporally_filtered_high_probability_fire_pixel
+# 34 temporally_filtered_medium_probability_fire_pixel
+# 35    temporally_filtered_low_probability_fire_pixel
+if(!file.exists("data/data_output/goes16-flag-mask-meanings.csv")) {
+  # Get example .nc file
+  ex_aws_path <- goes_af$aws_path[1]
+  ex_filename <- goes_af$filename[1]
+  ex_local_path <- "data/data_raw/goes16-example.nc"
+  
+  system2(command = "aws", args = glue::glue("s3 cp s3://noaa-goes16/{ex_aws_path} {ex_local_path} --no-sign-request"))
+  
+  nc <- ncdf4::nc_open(ex_local_path) %>% ncdf4::ncatt_get(varid = "Mask")
+  flag_vals <- nc[["flag_values"]]
+  flag_meanings <- nc[["flag_meanings"]] %>% str_split(pattern = " ", simplify = TRUE) %>% as.vector()
+  flag_df <- data.frame(flag_vals, flag_meanings)
+  
+  readr::write_csv(x = flag_df, file = "data/data_output/goes16-flag-mask-meanings.csv")
+}
+
+fire_flags <- 
+  readr::read_csv(file = "data/data_output/goes16-flag-mask-meanings.csv") %>% 
+  dplyr::filter(stringr::str_detect(flag_meanings, pattern = "_fire_pixel")) %>% 
+  dplyr::filter(stringr::str_detect(flag_meanings, pattern = "no_fire_pixel", negate = TRUE)) %>% 
+  dplyr::pull(flag_vals)
+
+# Function that downloads the next GOES-16 image, reads it into memory using the {terra} package
+get_goes_points <- function(aws_path, filename, scan_center, local_path) {
+  
+  # Round the image datetime to the nearest hour
+  rounded_datetime <- 
+    scan_center %>% 
+    lubridate::parse_date_time2(orders = "%Y%m%d%H%M%S") %>% # lubridate::ymd_hms() is failing me here for e.g., "2020052200050.9"
+    lubridate::round_date(scan_center, unit = "hour")
+  
+  rounded_datetime_txt <- 
+    paste0(lubridate::year(rounded_datetime),
+           stringr::str_pad(lubridate::month(rounded_datetime), width = 2, side = "left", pad = "0"),
+           stringr::str_pad(lubridate::day(rounded_datetime), width = 2, side = "left", pad = "0"),
+           stringr::str_pad(lubridate::hour(rounded_datetime), width = 2, side = "left", pad = "0"),
+           "00")
+  
+  system2(command = "aws", args = glue::glue("s3 cp s3://noaa-goes16/{aws_path} {local_path} --no-sign-request"), stdout = FALSE)
+  
+  # Read in the .nc file using the {terra} package in order to preserve CRS data and values properly (and its fast!)
+  goes <- terra::rast(local_path)
+  
+  # For joining to our curvilinear grid later, we also want to add a new raster layer with the cellindex values
+  cellindex <- goes["DQF"]
+  cellindex <- terra::setValues(x = cellindex, values = terra::cells(cellindex)) %>% stats::setNames("cellindex")
+  
+  # Stack the original .nc file with the `cellindex` raster layer
+  goes <- c(goes, cellindex)
+  
+  # Get the crs of the .nc file; This will be important later because the satellite moved in 2017
+  # from its initial testing position to its operational position, and so the raster cells
+  # are representing different areas on the Earth when that happened (encoded in the CRS though)
+  goes_crs <- terra::crs(goes)
+  
+  goes_modis_sinu <-
+    goes %>%
+    terra::as.data.frame(xy = TRUE) %>%
+    dplyr::filter(Mask %in% fire_flags) %>%
+    sf::st_as_sf(coords = c("x", "y"), crs = goes_crs, remove = FALSE) %>%
+    sf::st_transform("+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs") %>%
+    dplyr::mutate(scan_center = lubridate::parse_date_time2(x = scan_center, orders = "%Y%m%d%H%M%S")) %>%
+    dplyr::mutate(rounded_datetime = rounded_datetime) %>%
+    dplyr::select(scan_center, rounded_datetime, cellindex, x, y, dplyr::everything()) %>%
+    dplyr::mutate(sinu_x = sf::st_coordinates(.)[, 1],
+                  sinu_y = sf::st_coordinates(.)[, 2]) %>%
+    sf::st_drop_geometry()
+  
+  readr::write_csv(x = goes_modis_sinu, file = glue::glue("data/data_output/goes16/{rounded_datetime_txt}_{scan_center}_{filename}.csv"))
+  
+  system2(command = "aws", args = glue::glue("s3 cp data/data_output/goes16/{rounded_datetime_txt}_{scan_center}_{filename}.csv s3://earthlab-mkoontz/goes16/{rounded_datetime_txt}_{scan_center}_{filename}.csv"), stdout = FALSE)
+  
+  unlink(local_path)
+  unlink(glue::glue("data/data_output/goes16/{rounded_datetime_txt}_{scan_center}_{filename}.csv"))
+  
+  rm(goes)
+  rm(goes_modis_sinu)
+  rm(cellindex)
+  
+  return(NULL)
+}
+
+get_crs_info <- function(aws_path, filename, scan_center, local_path) {
+  
+  # Round the image datetime to the nearest hour
+  rounded_datetime <- 
+    scan_center %>% 
+    lubridate::parse_date_time2(orders = "%Y%m%d%H%M%S") %>% # lubridate::ymd_hms() is failing me here for e.g., "2020052200050.9"
+    lubridate::round_date(scan_center, unit = "hour")
+  
+  rounded_datetime_txt <- 
+    paste0(lubridate::year(rounded_datetime),
+           stringr::str_pad(lubridate::month(rounded_datetime), width = 2, side = "left", pad = "0"),
+           stringr::str_pad(lubridate::day(rounded_datetime), width = 2, side = "left", pad = "0"),
+           stringr::str_pad(lubridate::hour(rounded_datetime), width = 2, side = "left", pad = "0"),
+           "00")
+  
+  # Read in the .nc file using the {terra} package in order to preserve CRS data and values properly (and its fast!)
+  goes <- terra::rast(local_path)
+  
+  # Get the crs of the .nc file; This will be important later because the satellite moved in 2017
+  # from its initial testing position to its operational position, and so the raster cells
+  # are representing different areas on the Earth when that happened (encoded in the CRS though)
+  goes_crs <- terra::crs(goes)
+  
+  # Record CRS data for this particular goes image
+  out <-
+    tibble::tibble(scan_center = scan_center,
+                   filename = glue::glue("{rounded_datetime_txt}_{scan_center}_{filename}.csv"),
+                   local_path = local_path,
+                   aws_path = aws_path,
+                   goes_crs = goes_crs)
+  return(crs_table)
+  
+}
+
+# Create directory to hold the raw GOES-16 active fire data until it gets deleted
+dir.create("data/data_raw/goes16/", showWarnings = FALSE)
+
+# Create directory to hold all the processed GOES-16 active fire data
+dir.create("data/data_output/goes16/", showWarnings = FALSE)
+
+# Get the file names of the data that have already been processed
+processed_goes <- 
+  tibble::tibble(aws_files_raw = system2(command = "aws", args = glue::glue("s3 ls s3://earthlab-mkoontz/goes16/ --recursive"), stdout = TRUE)) %>% 
+  dplyr::filter(nchar(aws_files_raw) == 139) %>% 
+  dplyr::mutate(filename_full = stringr::str_sub(string = aws_files_raw, start = 39),
+                filename = stringr::str_sub(string = filename_full, start = 29, end = -5))
+
+# going for a parallelized parallelization approach
+# Divide data into 4 separate batches, work on a different EC2 instance for each
+# Parallelize on each of the EC2 instances
+
+# divide the goes_af into batches
+n_batches <- 1
+n_subbatches <- 20 # number of cores
+
+base::set.seed(1959)
+# Only need to process the GOES file if processed data don't yet exist
+batches <- 
+  goes_af %>% 
+  dplyr::mutate(filebase = stringr::str_sub(string = filename, start = 1, end = -4)) %>% 
+  dplyr::filter(!(filebase %in% processed_goes$filename)) %>% 
+  dplyr::filter(!(filebase %in% c("OR_ABI-L2-FDCF-M3_G16_s20172632115407_e20172632126173_c20172632126283",
+                                 "OR_ABI-L2-FDCF-M3_G16_s20181231215382_e20181231226149_c20181231226258",
+                                 "OR_ABI-L2-FDCF-M3_G16_s20183241845341_e20183241856108_c20183241856213",
+                                 "OR_ABI-L2-FDCF-M6_G16_s20202471650186_e20202471659494_c20202471700318"))) %>% 
+  base::split(f = sample(1:n_batches, size = nrow(.), replace = TRUE))
+
+# multicore processing for batch j
+j <- 1
+
+subbatches <- 
+  batches[[j]] %>% 
+  base::split(f = sample(1:n_subbatches, size = nrow(.), replace = TRUE))
+
+(start <- Sys.time())
+
+future::plan(strategy = "multiprocess", workers = n_subbatches)
+
+furrr::future_map(.x = subbatches, .f = function(this_batch) {
+  
+  out <- 
+    this_batch %>% 
+    slider::slide(.f = ~ .) %>%  # Using slider::slide() as a rowwise iterator
+    purrr::map(.f = function(this_goes) {
+      
+      aws_path <- this_goes$aws_path
+      filename <- stringr::str_sub(this_goes$filename, start = 1, end = -4)
+      scan_center <- this_goes$scan_center
+      local_path <- glue::glue("data/data_raw/goes16/{scan_center}_{filename}.nc")
+      
+      get_goes_points(aws_path, filename, scan_center, local_path)
+      
+    })
+  
+})
+
+(difftime(Sys.time(), start))
---
+++ SI-table-1_nighttime-fire-regime_detections-and-FRP.R
@@ -0,0 +1,299 @@
+# Defining nighttime fire regime by FRP and detections
+
+library(tidyverse)
+library(raster)
+library(data.table)
+library(viridis)
+
+
+# gridded MCD14ML data ---------------------------------------------------------
+
+if(length(list.files("data/data_output/CSV_nocorn_grid_0_25_degree_vars/AFC_num/")) == 0) {
+  download.file(url = "http://earthlab-jmcglinchy.s3-us-west-2.amazonaws.com/night_fire/gridded/vars_CSV_nocorn.zip", 
+                destfile = "data/data_output/vars_CSV_nocorn.zip", 
+                method = "curl")
+  
+  unzip(zipfile = "data/data_output/vars_CSV_nocorn.zip", exdir = "data/data_output/")
+}
+
+
+# fire detection count ----------------------------------------------------
+
+afd_files <- list.files("data/data_output/CSV_nocorn_grid_0_25_degree_vars/AFC_num/") %>% 
+  tibble::enframe(name = NULL) %>% 
+  setNames("filename") %>% 
+  tidyr::separate(col = filename, into = c("satellite", "daynight", "afc", "datatype", "month", "year", "fileextension"), remove = FALSE) %>% 
+  dplyr::select(-satellite, -afc, -datatype, -fileextension) %>% 
+  dplyr::mutate(year = as.numeric(year),
+                daynight = ifelse(daynight == "D", yes = "day", no = "night")) %>% 
+  dplyr::filter(year >= 2003)
+
+read_and_stack_rasters <-  function(filename, daynight, month, year) {
+  r <- raster::raster(paste0("data/data_output/CSV_nocorn_grid_0_25_degree_vars/AFC_num/", filename))
+  return(r)
+}
+
+# Stack all 192 year-month rasters representing total detections
+# and sum them
+if (!file.exists("data/data_output/total-detections_day_0.25.tif")) {
+  afd_day <-
+    afd_files %>% 
+    dplyr::filter(daynight == "day") %>% 
+    purrr::pmap(.f = read_and_stack_rasters) %>% 
+    do.call("stack", .) %>% 
+    raster::calc(fun = sum)
+  
+  # Line up the rasters with the GLDAS landcover raster
+  afd_day <- raster::shift(x = afd_day, dx = -0.125, dy = 0.125)
+  
+  raster::writeRaster(x = afd_day, filename = "data/data_output/total-detections_day_0.25.tif", overwrite = TRUE)
+}
+
+# Repeat for night detections rasters
+if (!file.exists("data/data_output/total-detections_night_0.25.tif")) {
+  afd_night <-
+    afd_files %>% 
+    dplyr::filter(daynight == "night") %>% 
+    purrr::pmap(.f = read_and_stack_rasters) %>% 
+    do.call("stack", .) %>% 
+    raster::calc(fun = sum)
+  
+  # Line up the rasters with the GLDAS landcover raster
+  afd_night <- raster::shift(x = afd_night, dx = -0.125, dy = 0.125)
+  
+  raster::writeRaster(x = afd_night, filename = "data/data_output/total-detections_night_0.25.tif", overwrite = TRUE)
+}
+
+afd_day <- raster::raster("data/data_output/total-detections_day_0.25.tif")
+afd_night <- raster::raster("data/data_output/total-detections_night_0.25.tif")
+
+# FRP data ----------------------------------------------------------------
+
+frp_files <- list.files("data/data_output/CSV_nocorn_grid_0_25_degree_vars/FRP_total/") %>% 
+  tibble::enframe(name = NULL) %>% 
+  setNames("filename") %>% 
+  tidyr::separate(col = filename, into = c("satellite", "daynight", "afc", "datatype", "month", "year", "fileextension"), remove = FALSE) %>% 
+  dplyr::select(-satellite, -afc, -datatype, -fileextension) %>% 
+  dplyr::mutate(year = as.numeric(year),
+                daynight = ifelse(daynight == "D", yes = "day", no = "night")) %>% 
+  dplyr::filter(year >= 2003)
+
+read_and_stack_rasters <-  function(filename, daynight, month, year) {
+  r <- raster::raster(paste0("data/data_output/CSV_nocorn_grid_0_25_degree_vars/FRP_total/", filename))
+  return(r)
+}
+
+if(!file.exists("data/data_output/frp_day_0.25.tif")) {
+  # Stack all 192 year-month rasters representing total FRP
+  # and then sum them
+  frp_day <-
+    frp_files %>% 
+    dplyr::filter(daynight == "day") %>% 
+    purrr::pmap(.f = read_and_stack_rasters) %>% 
+    do.call("stack", .) %>% 
+    raster::calc(fun = sum)
+  
+  # Line up the rasters with the GLDAS landcover raster
+  frp_day <- raster::shift(x = frp_day, dx = -0.125, dy = 0.125)
+  
+  raster::writeRaster(x = frp_day, filename = "data/data_output/frp_day_0.25.tif", overwrite = TRUE)
+}
+
+if(!file.exists("data/data_output/frp_night_0.25.tif")) {
+  # Repeat for the night rasters
+  frp_night <-
+    frp_files %>% 
+    dplyr::filter(daynight == "night") %>% 
+    purrr::pmap(.f = read_and_stack_rasters) %>% 
+    do.call("stack", .) %>% 
+    raster::calc(fun = sum)
+  
+  # Line up the rasters with the GLDAS landcover raster
+  frp_night <- raster::shift(x = frp_night, dx = -0.125, dy = 0.125)
+  
+  raster::writeRaster(x = frp_night, filename = "data/data_output/frp_night_0.25.tif", overwrite = TRUE)
+}
+
+frp_day <- raster::raster("data/data_output/frp_day_0.25.tif")
+frp_night <- raster::raster("data/data_output/frp_night_0.25.tif")
+
+# overpass count ----------------------------------------------------------
+
+if(!file.exists("data/data_output/2003-2018_day_overpass-count.tif")) {
+  download.file(url = "https://earthlab-mkoontz.s3-us-west-2.amazonaws.com/MODIS-overpass-counts_0.25_analysis-ready/2003-2018/2003-2018_day_overpass-count.tif",
+                destfile = "data/data_output/2003-2018_day_overpass-count.tif",
+                method = "curl")
+}
+
+if(!file.exists("data/data_output/2003-2018_night_overpass-count.tif")) {
+  download.file(url = "https://earthlab-mkoontz.s3-us-west-2.amazonaws.com/MODIS-overpass-counts_0.25_analysis-ready/2003-2018/2003-2018_night_overpass-count.tif",
+                destfile = "data/data_output/2003-2018_night_overpass-count.tif",
+                method = "curl")
+}
+
+# Represents count of all day (or night) overpasses between 2003 and 2018
+day_overpass_count <- raster::raster("data/data_output/2003-2018_day_overpass-count.tif")
+night_overpass_count <- raster::raster("data/data_output/2003-2018_night_overpass-count.tif")
+
+day_overpass_count <- raster::shift(day_overpass_count, dx = -0.125, dy = 0.125)
+night_overpass_count <- raster::shift(night_overpass_count, dx = -0.125, dy = 0.125)
+
+# landcover ---------------------------------------------------------------
+
+# The lookup table to convert landcover index (in the raster) to
+# the landcover name
+landcover_table <- 
+  readr::read_csv(file = "data/data_raw/GLDASp4_domveg_025d_lookup-table.csv")
+
+# Just use the 11 landcovers that we used to derive our VPDt
+landcovers_of_interest <- c(1:2, 4:10, 12, 14)
+# landcovers_of_interest <- c(18:20) # Just tundra
+
+landcover <- raster::raster(x = "data/data_raw/GLDASp4_domveg_025d.nc4")
+# In the 4326 coordinate reference system, pixels have different
+# areas, so we need to account for that as we do aggregations of the
+# gridded product
+landcover_area <- raster::area(landcover)
+s <- stack(landcover, landcover_area)
+
+
+# get all the values of the landcover raster including the lon/lat
+# coordinates so we can join it with the 0.25 grid fire detections and
+# frp gridded data
+landcover_df <- 
+  s %>% 
+  as.data.frame(xy = TRUE) %>% 
+  setNames(c("lon", "lat", "index", "pixel_area_km2")) %>% 
+  dplyr::left_join(landcover_table, by = "index")
+
+landcover_DT <- as.data.table(landcover_df)
+data.table::setkeyv(landcover_DT, cols = c("lon", "lat"))
+
+total_landcover_areas <-
+  landcover_DT %>% 
+  dplyr::group_by(index) %>% 
+  dplyr::summarize(area_km2 = as.numeric(sum(pixel_area_km2, na.rm = TRUE))) %>% 
+  dplyr::left_join(landcover_table, by = "index")
+
+# Convert landcover areas to a data.table for joining
+total_landcover_areas_DT <- as.data.table(total_landcover_areas)
+data.table::setkeyv(total_landcover_areas_DT, cols = c("index"))
+
+# correct for overpasses --------------------------------------------------
+day_DT <-
+  raster::stack(afd_day, frp_day, day_overpass_count) %>% 
+  as.data.frame(xy = TRUE) %>% 
+  setNames(c("lon", "lat", "total_detections", "frp", "overpasses")) %>% 
+  dplyr::mutate(daynight = "day") %>% 
+  dplyr::filter(lat >= -90) %>% 
+  as.data.table()
+
+night_DT <-
+  raster::stack(afd_night, frp_night, night_overpass_count) %>% 
+  as.data.frame(xy = TRUE) %>% 
+  setNames(c("lon", "lat", "total_detections", "frp", "overpasses")) %>% 
+  dplyr::mutate(daynight = "night") %>% 
+  dplyr::filter(lat >= -90) %>% 
+  as.data.table()
+
+daynight_DT <- rbind(day_DT, night_DT)
+
+daynight_DT <- daynight_DT[, `:=`(detections_per_overpass = total_detections / overpasses,
+                                  frp_per_overpass = frp / overpasses)]
+
+daynight_DT <- daynight_DT[!is.na(detections_per_overpass)]
+data.table::setkey(x = daynight_DT, lon, lat, daynight)
+
+# join fire product with landcover -----------------------------------------------------
+
+lon_lat_landcover <- landcover_DT[daynight_DT]
+
+# lon_lat_landcover[, `:=`(detections_per_overpass_per_km2 = total_detections / overpasses / pixel_area_km2,
+#                          frp_per_overpass_per_km2 = frp / overpasses / pixel_area_km2)]
+
+
+# summarize across all pixels belonging to each landcover type (and daynight)
+afd_frp_landcover <- lon_lat_landcover[index %in% landcovers_of_interest & detections_per_overpass != Inf, .(detections_per_overpass = sum(detections_per_overpass),
+                                                                                                             frp_per_overpass = sum(frp_per_overpass)),
+                                       by = .(index, daynight)]
+
+afd_frp_landcover <- total_landcover_areas_DT[afd_frp_landcover]
+
+afd_frp_landcover <- afd_frp_landcover[, `:=`(detections_per_overpass_per_1e6km2 = 1e6 * detections_per_overpass / area_km2,
+                                              frp_per_overpass_per_1e6km2 = 1e6 * frp_per_overpass / area_km2)]
+
+#####
+# summarize across all pixels belonging to burnable landcover types (and daynight)
+afd_frp_just_daynight <- lon_lat_landcover[index %in% landcovers_of_interest & detections_per_overpass != Inf, .(detections_per_overpass = sum(detections_per_overpass),
+                                                                                                                 frp_per_overpass = sum(frp_per_overpass)),
+                                           by = .(daynight)]
+
+total_burnable_area_km2 <-
+  total_landcover_areas %>% 
+  dplyr::filter(index %in% landcovers_of_interest) %>% 
+  dplyr::summarize(area_km2 = sum(area_km2)) %>% 
+  pull()
+
+afd_frp_just_daynight$area_km2 <- total_burnable_area_km2
+
+afd_frp_just_daynight <- afd_frp_just_daynight[, `:=`(detections_per_overpass_per_1e6km2 = 1e6 * detections_per_overpass / area_km2,
+                                                      frp_per_overpass_per_1e6km2 = 1e6 * frp_per_overpass / area_km2)]
+
+# pivot wider to put similar day and night measures in separate columns (so we can get
+# percent)
+afd_frp_landcover_wide <-
+  afd_frp_landcover %>% 
+  dplyr::select(index, daynight, detections_per_overpass_per_1e6km2, frp_per_overpass_per_1e6km2) %>% 
+  tidyr::pivot_wider(names_from = "daynight", values_from = c("detections_per_overpass_per_1e6km2", "frp_per_overpass_per_1e6km2")) %>% 
+  dplyr::mutate(pct_night_afd = 100 * detections_per_overpass_per_1e6km2_night / (detections_per_overpass_per_1e6km2_night + detections_per_overpass_per_1e6km2_day),
+                pct_night_frp = 100 * frp_per_overpass_per_1e6km2_night / (frp_per_overpass_per_1e6km2_night + frp_per_overpass_per_1e6km2_day))
+
+
+afd_frp_just_daynight_wide <-
+  afd_frp_just_daynight %>% 
+  dplyr::select(daynight, area_km2, detections_per_overpass_per_1e6km2, frp_per_overpass_per_1e6km2) %>% 
+  dplyr::mutate(landcover = "All Burnable Landcovers",
+                area_1e6km2 = area_km2 / 1e6) %>% 
+  tidyr::pivot_wider(names_from = "daynight", values_from = c("detections_per_overpass_per_1e6km2", "frp_per_overpass_per_1e6km2")) %>% 
+  dplyr::mutate(pct_night_afd = 100 * detections_per_overpass_per_1e6km2_night / (detections_per_overpass_per_1e6km2_night + detections_per_overpass_per_1e6km2_day),
+                pct_night_frp = 100 * frp_per_overpass_per_1e6km2_night / (frp_per_overpass_per_1e6km2_night + frp_per_overpass_per_1e6km2_day))
+
+# Make table suitable for printing
+# final row to be appended as the "total"
+
+total_row <-
+  afd_frp_just_daynight_wide %>% 
+  dplyr::select(landcover, 
+                area_1e6km2,
+                detections_per_overpass_per_1e6km2_day, 
+                detections_per_overpass_per_1e6km2_night,
+                pct_night_afd,
+                frp_per_overpass_per_1e6km2_day,
+                frp_per_overpass_per_1e6km2_night, 
+                pct_night_frp) %>% 
+  dplyr::mutate_if(is.numeric, sprintf, fmt = "%.1f")
+
+
+night_fire_regime_table <-
+  afd_frp_landcover_wide %>% 
+  dplyr::left_join(total_landcover_areas) %>% 
+  dplyr::mutate(area_1e6km2 = area_km2 / 1e6) %>% 
+  dplyr::filter(index %in% landcovers_of_interest & index != 20) %>% 
+  # dplyr::mutate(landcover = factor(landcover, levels = c("Evergreen Needleleaf Forest, Evergreen Broadleaf Forest, Deciduous Broadleaf Forest, Mixed Forest, Closed Shrublands, Open Shrublands, Woody Savannas, Savannas, Grassland, Cropland, Cropland/Natural Vegetation Mosaic"))) %>% 
+  dplyr::arrange(dplyr::desc(pct_night_afd)) %>% 
+  dplyr::select(-index) %>% 
+  dplyr::select(landcover, 
+                area_1e6km2,
+                detections_per_overpass_per_1e6km2_day, 
+                detections_per_overpass_per_1e6km2_night,
+                pct_night_afd,
+                frp_per_overpass_per_1e6km2_day,
+                frp_per_overpass_per_1e6km2_night, 
+                pct_night_frp) %>% 
+  dplyr::mutate_if(is.numeric, sprintf, fmt = "%.1f") %>% 
+  rbind(total_row)
+
+
+night_fire_regime_table
+
+readr::write_csv(x = night_fire_regime_table, path = "tables/night-fire-regime-table.csv")

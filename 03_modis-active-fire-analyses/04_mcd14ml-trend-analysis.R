a57efb2a46d63dbbf70ea3ba3ba8a1a2892fc3ab
Michael Koontz <mikoontz@gmail.com>
2021-05-17 16:01:40 -0600

    global summaries and fix frp plots

--- 03_modis-active-fire-analyses/04_mcd14ml-trend-analysis.R
+++ 03_modis-active-fire-analyses/04_mcd14ml-trend-analysis.R
@@ -15,7 +15,9 @@ lc <- dplyr::left_join(x = lc_area, y = lc_lookup, by = c(lc = "koppen_modis_cod
 afd <- data.table::fread(input = "data/out/mcd14ml_n-frp_month-lc-xy-daynight-summary.csv")
 
 afd[, `:=`(n_per_op = ifelse(dn_detect == "day", yes = n / op_day, no = n / op_night),
-           frp_per_op = ifelse(dn_detect == "day", yes = sum_frp / op_day, no = n / op_night))]
+           frp_per_op = ifelse(dn_detect == "day", yes = sum_frp / op_day, no = sum_frp / op_night))]
+
+afd <- afd[!(op_night == 0 & dn_detect == "night"), ][!(op_day == 0 & dn_detect == "day"), ]
 
 afd_summary <- 
   afd[, .(n_per_op = mean(n_per_op), frp_per_op = mean(frp_per_op)), by = .(acq_year, acq_month, dn_detect, lc)] %>% 
@@ -30,6 +32,46 @@ afd_summary <-
 
 afd_summary
 
+afd_global_summary <-
+  afd[, .(n_per_op = mean(n_per_op), frp_per_op = mean(frp_per_op)), by = .(acq_year, acq_month, dn_detect)] %>% 
+  dplyr::mutate(year_month = lubridate::ymd(paste0(acq_year, "-", acq_month, "-", "15"))) %>% 
+  tidyr::complete(year_month, nesting(dn_detect), fill = list(n_per_op = 0, frp_per_op = 0)) %>% 
+  dplyr::mutate(n_per_op_per_Mkm2 = (n_per_op / sum(lc$area_km2)) * 1e6,
+                frp_per_op_per_Mkm2 = (frp_per_op / sum(lc$area_km2)) * 1e6) %>% 
+  dplyr::filter(acq_year >= 2003)
+
+afd_global_summary_wide <-
+  afd_global_summary %>% 
+  tidyr::pivot_wider(names_from = "dn_detect", values_from = c("n_per_op", "frp_per_op"), id_cols = c("year_month", "acq_year", "acq_month")) %>% 
+  dplyr::mutate(prop_n_night = n_per_op_night  / (n_per_op_night + n_per_op_day))
+
+global_n_gg <-
+  ggplot(afd_global_summary, aes(x = year_month, y = n_per_op, color = dn_detect)) +
+  geom_line() +
+  geom_smooth() +
+  theme_bw()
+
+ggsave(filename = "figs/mcd14ml_n-trend_global.png", plot = global_n_gg)
+
+global_frp_gg <-
+  ggplot(afd_global_summary, aes(x = year_month, y = frp_per_op, color = dn_detect)) +
+  geom_line() +
+  geom_smooth() +
+  theme_bw()
+
+ggsave(filename = "figs/mcd14ml_frp-trend_global.png", plot = global_frp_gg)
+
+global_prop_n_gg <-
+  ggplot(afd_global_summary_wide, aes(x = year_month, y = prop_n_night)) +
+  geom_line() +
+  geom_smooth() +
+  labs(x = "Date (monthly increments)",
+       y = "Proportion of global MODIS detections at night") +
+  theme_bw()
+
+global_prop_n_gg
+
+ggsave(filename = "figs/mcd14ml_prop-n-trend_global.png", plot = global_prop_n_gg)
 # number of detections
 # all
 # ggplot(afd_summary, aes(x = year_month, y = n_per_op_per_Mkm2, color = dn_detect)) +

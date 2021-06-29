323ce901cc183c7df9c46fe13adcc83381c79fe7
Michael Koontz <mikoontz@gmail.com>
2021-06-28 21:47:22 -0600

    make it runnable on a fresh machine

--- 02_model-vpd-thresholds-using-goes/02_get-af-data_aws.R
+++ 02_model-vpd-thresholds-using-goes/02_get-af-data_aws.R
@@ -7,6 +7,8 @@ library(purrr)
 library(furrr)
 library(future)
 
+dir.create("data/out/goes16/", showWarnings = FALSE)
+
 get_latest_goes <- FALSE
 
 if(get_latest_goes | !file.exists("data/out/goes16-filenames.csv")) {
@@ -14,7 +16,7 @@ if(get_latest_goes | !file.exists("data/out/goes16-filenames.csv")) {
 }  
 
 # Read in the GOES metadata acquired from Amazon Earth using get-af-metadata.R script
-goes_af <- read.csv("data/out/goes16-filenames.csv")
+goes_af <- read.csv("data/out/goes16-filenames.csv", colClasses = c(scan_center = "character"))
 
 # Get the flag values that are important using an example .nc file if not done already
 # flag_vals                            flag_meanings
@@ -32,11 +34,11 @@ goes_af <- read.csv("data/out/goes16-filenames.csv")
 # 35    temporally_filtered_low_probability_fire_pixel
 if(!file.exists("data/out/goes16-flag-mask-meanings.csv")) {
   # Get example .nc file
-  ex_aws_path <- goes_af$aws_path[1]
-  ex_filename <- goes_af$filename[1]
+  ex_aws_path <- goes_af$fullname[1]
+  ex_filename <- goes_af$fullname[1]
   ex_local_path <- "data/raw/goes16-example.nc"
   
-  system2(command = "aws", args = glue::glue("s3 cp s3://noaa-goes16/{ex_aws_path} {ex_local_path} --no-sign-request"))
+  system2(command = "aws", args = glue::glue("s3 cp s3://noaa-goes16/ABI-L2-FDCF/{ex_aws_path} {ex_local_path} --no-sign-request"))
   
   nc <- ncdf4::nc_open(ex_local_path) %>% ncdf4::ncatt_get(varid = "Mask")
   flag_vals <- nc[["flag_values"]]
@@ -52,116 +54,58 @@ fire_flags <-
   dplyr::filter(stringr::str_detect(flag_meanings, pattern = "no_fire_pixel", negate = TRUE)) %>% 
   dplyr::pull(flag_vals)
 
-# Function that downloads the next GOES-16 image, reads it into memory using the {terra} package
-# get_goes_points <- function(aws_path, filename, scan_center, local_path) {
+# Function that reads each GOES-16 image using the {terra} package, subsets
+# out the active fire data, and writes it to a .csv
 get_goes_points <- function(this_batch) {
-    
-  out <- vector(mode = "list", length = nrow(this_batch))
   
   for (k in 1:nrow(this_batch)) {
     # Read in the .nc file using the {terra} package in order to preserve CRS data and values properly (and its fast!)
-    
     goes <- terra::rast(glue::glue("data/raw/goes16/{this_batch$fullname[k]}"))
+    scan_center <- this_batch$scan_center[k]
+    rounded_datetime <- this_batch$rounded_datetime[k]
+    rounded_datetime_txt <- this_batch$rounded_datetime_txt[k]
+    filename <- this_batch$filename[k]
     
-    # Round the image datetime to the nearest hour
-  rounded_datetime <- 
-    scan_center %>% 
-    lubridate::parse_date_time2(orders = "%Y%m%d%H%M%S") %>% # lubridate::ymd_hms() is failing me here for e.g., "2020052200050.9"
-    lubridate::round_date(scan_center, unit = "hour")
-  
-  rounded_datetime_txt <- 
-    paste0(lubridate::year(rounded_datetime),
-           stringr::str_pad(lubridate::month(rounded_datetime), width = 2, side = "left", pad = "0"),
-           stringr::str_pad(lubridate::day(rounded_datetime), width = 2, side = "left", pad = "0"),
-           stringr::str_pad(lubridate::hour(rounded_datetime), width = 2, side = "left", pad = "0"),
-           "00")
-  
-  system2(command = "aws", args = glue::glue("s3 cp s3://noaa-goes16/{aws_path} {local_path} --no-sign-request"), stdout = FALSE)
-  
-  # Read in the .nc file using the {terra} package in order to preserve CRS data and values properly (and its fast!)
-  goes <- terra::rast(local_path)
-  
-  # For joining to our curvilinear grid later, we also want to add a new raster layer with the cellindex values
-  cellindex <- goes["DQF"]
-  cellindex <- terra::setValues(x = cellindex, values = terra::cells(cellindex)) %>% stats::setNames("cellindex")
-  
-  # Stack the original .nc file with the `cellindex` raster layer
-  goes <- c(goes, cellindex)
-  
-  # Get the crs of the .nc file; This will be important later because the satellite moved in 2017
-  # from its initial testing position to its operational position, and so the raster cells
-  # are representing different areas on the Earth when that happened (encoded in the CRS though)
-  goes_crs <- terra::crs(goes)
-  
-  goes_modis_sinu <-
-    goes %>%
-    terra::as.data.frame(xy = TRUE) %>%
-    dplyr::filter(Mask %in% fire_flags) %>%
-    sf::st_as_sf(coords = c("x", "y"), crs = goes_crs, remove = FALSE) %>%
-    sf::st_transform("+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs") %>%
-    dplyr::mutate(scan_center = lubridate::parse_date_time2(x = scan_center, orders = "%Y%m%d%H%M%S")) %>%
-    dplyr::mutate(rounded_datetime = rounded_datetime) %>%
-    dplyr::select(scan_center, rounded_datetime, cellindex, x, y, dplyr::everything()) %>%
-    dplyr::mutate(sinu_x = sf::st_coordinates(.)[, 1],
-                  sinu_y = sf::st_coordinates(.)[, 2]) %>%
-    sf::st_drop_geometry()
-  
-  readr::write_csv(x = goes_modis_sinu, file = glue::glue("data/out/goes16/{rounded_datetime_txt}_{scan_center}_{filename}.csv"))
-  
-  system2(command = "aws", args = glue::glue("s3 cp data/out/goes16/{rounded_datetime_txt}_{scan_center}_{filename}.csv s3://earthlab-mkoontz/goes16/{rounded_datetime_txt}_{scan_center}_{filename}.csv"), stdout = FALSE)
-  
-  unlink(local_path)
-  unlink(glue::glue("data/out/goes16/{rounded_datetime_txt}_{scan_center}_{filename}.csv"))
-  
-  rm(goes)
-  rm(goes_modis_sinu)
-  rm(cellindex)
+    # For joining to our curvilinear grid later, if we wish, we also want to add a
+    # new raster layer with the cellindex values
+    cellindex <- goes["DQF"]
+    cellindex <- terra::setValues(x = cellindex, values = terra::cells(cellindex)) %>% stats::setNames("cellindex")
+    
+    # Stack the original .nc file with the `cellindex` raster layer
+    goes <- c(goes, cellindex)
+    
+    # Get the crs of the .nc file; This will be important later because the satellite moved in 2017
+    # from its initial testing position to its operational position, and so the raster cells
+    # are representing different areas on the Earth when that happened (encoded in the CRS though)
+    goes_crs <- terra::crs(goes)
+    
+    goes_modis_sinu <-
+      goes %>%
+      terra::as.data.frame(xy = TRUE) %>%
+      dplyr::filter(Mask %in% fire_flags) %>%
+      sf::st_as_sf(coords = c("x", "y"), crs = goes_crs, remove = FALSE) %>%
+      sf::st_transform("+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs") %>%
+      dplyr::mutate(scan_center = scan_center) %>%
+      dplyr::mutate(rounded_datetime = rounded_datetime) %>%
+      dplyr::select(scan_center, rounded_datetime, cellindex, x, y, dplyr::everything()) %>%
+      dplyr::mutate(sinu_x = sf::st_coordinates(.)[, 1],
+                    sinu_y = sf::st_coordinates(.)[, 2]) %>%
+      sf::st_drop_geometry()
+    
+    readr::write_csv(x = goes_modis_sinu, file = glue::glue("data/out/goes16/{rounded_datetime_txt}_{scan_center}_{filename}.csv"))
+    
+    # unlink(this_batch$fullname[k])
+    # unlink(glue::glue("data/out/goes16/{rounded_datetime_txt}_{scan_center}_{filename}.csv"))
+    # 
+    # rm(goes)
+    # rm(goes_modis_sinu)
+    # rm(cellindex)
   }
   
-  out[[k]] <- dplyr::tibble(local_path_full = this_batch$local_path_full[k], processed_filename = this_batch$processed_filename[k], crs = this_crs)
+  system2(command = "aws", args = glue::glue("s3 sync data/out/goes16/ s3://earthlab-mkoontz/goes16/"))
   
   return(NULL)
 }
-
-get_crs_info <- function(aws_path, filename, scan_center, local_path) {
-  
-  # Round the image datetime to the nearest hour
-  rounded_datetime <- 
-    scan_center %>% 
-    lubridate::parse_date_time2(orders = "%Y%m%d%H%M%S") %>% # lubridate::ymd_hms() is failing me here for e.g., "2020052200050.9"
-    lubridate::round_date(scan_center, unit = "hour")
-  
-  rounded_datetime_txt <- 
-    paste0(lubridate::year(rounded_datetime),
-           stringr::str_pad(lubridate::month(rounded_datetime), width = 2, side = "left", pad = "0"),
-           stringr::str_pad(lubridate::day(rounded_datetime), width = 2, side = "left", pad = "0"),
-           stringr::str_pad(lubridate::hour(rounded_datetime), width = 2, side = "left", pad = "0"),
-           "00")
-  
-  # Read in the .nc file using the {terra} package in order to preserve CRS data and values properly (and its fast!)
-  goes <- terra::rast(local_path)
-  
-  # Get the crs of the .nc file; This will be important later because the satellite moved in 2017
-  # from its initial testing position to its operational position, and so the raster cells
-  # are representing different areas on the Earth when that happened (encoded in the CRS though)
-  goes_crs <- terra::crs(goes)
-  
-  # Record CRS data for this particular goes image
-  out <-
-    tibble::tibble(scan_center = scan_center,
-                   filename = glue::glue("{rounded_datetime_txt}_{scan_center}_{filename}.csv"),
-                   local_path = local_path,
-                   aws_path = aws_path,
-                   goes_crs = goes_crs)
-  return(crs_table)
-  
-}
-
-# Create directory to hold the raw GOES-16 active fire data until it gets deleted
-dir.create("data/raw/goes16/", showWarnings = FALSE)
-
-# Create directory to hold all the processed GOES-16 active fire data
-dir.create("data/out/goes16/", showWarnings = FALSE)
 
 # Get the file names of the data that have already been processed
 processed_goes <- 
@@ -174,9 +118,10 @@ processed_goes <-
 # Divide data into 4 separate batches, work on a different EC2 instance for each
 # Parallelize on each of the EC2 instances
 
-# divide the goes_af into batches
-n_batches <- 20
-# n_subbatches <- 20 # number of cores
+# number of cores on machine
+n_cores <- 4
+# precision of the progress bar
+pb_precision <- 100
 
 base::set.seed(1959)
 # Only need to process the GOES file if processed data don't yet exist
@@ -185,57 +130,21 @@ batches <-
   dplyr::mutate(filebase = stringr::str_sub(string = filename, start = 1, end = -4)) %>% 
   dplyr::filter(!(filebase %in% processed_goes$filename)) %>% 
   dplyr::filter(!(filebase %in% c("OR_ABI-L2-FDCF-M3_G16_s20172632115407_e20172632126173_c20172632126283",
-                                 "OR_ABI-L2-FDCF-M3_G16_s20181231215382_e20181231226149_c20181231226258",
-                                 "OR_ABI-L2-FDCF-M3_G16_s20183241845341_e20183241856108_c20183241856213",
-                                 "OR_ABI-L2-FDCF-M6_G16_s20202471650186_e20202471659494_c20202471700318"))) %>% 
+                                  "OR_ABI-L2-FDCF-M3_G16_s20181231215382_e20181231226149_c20181231226258",
+                                  "OR_ABI-L2-FDCF-M3_G16_s20183241845341_e20183241856108_c20183241856213",
+                                  "OR_ABI-L2-FDCF-M6_G16_s20202471650186_e20202471659494_c20202471700318"))) %>% 
   dplyr::mutate(filename = stringr::str_sub(filename, start = 1, end = -4),
                 local_path = glue::glue("data/raw/goes16/{scan_center}_{filename}.nc")) %>% 
   # lubridate::ymd_hms() is failing me here for e.g., "2020052200050.9"
-  dplyr::mutate(rounded_datetime = lubridate::round_date(lubridate::parse_date_time2(scan_center, orders = "%Y%m%d%H%M%S"), unit = "hour")
-) %>% 
-  base::split(f = sample(1:n_batches, size = nrow(.), replace = TRUE))
+  dplyr::mutate(rounded_datetime = lubridate::round_date(lubridate::parse_date_time2(scan_center, orders = "%Y%m%d%H%M%S"), unit = "hour"),
+                rounded_datetime_txt = paste0(lubridate::year(rounded_datetime),
+                                              stringr::str_pad(lubridate::month(rounded_datetime), width = 2, side = "left", pad = "0"),
+                                              stringr::str_pad(lubridate::day(rounded_datetime), width = 2, side = "left", pad = "0"),
+                                              stringr::str_pad(lubridate::hour(rounded_datetime), width = 2, side = "left", pad = "0"),
+                                              "00")) %>% 
+  base::split(f = sample(1:(pb_precision), size = nrow(.), replace = TRUE))
 
-
-lubridate::round_date(lubridate::parse_date_time2(scan_center, orders = "%Y%m%d%H%M%S"), unit = "hour")
-
-%>% 
-  # lubridate::ymd_hms() is failing me here for e.g., "2020052200050.9"
-  lubridate::round_date(scan_center, unit = "hour")
-
-rounded_datetime_txt <- 
-  paste0(lubridate::year(rounded_datetime),
-         stringr::str_pad(lubridate::month(rounded_datetime), width = 2, side = "left", pad = "0"),
-         stringr::str_pad(lubridate::day(rounded_datetime), width = 2, side = "left", pad = "0"),
-         stringr::str_pad(lubridate::hour(rounded_datetime), width = 2, side = "left", pad = "0"),
-         "00")
-
-# multicore processing for batch j
-j <- 1
-
-subbatches <- 
-  batches[[j]] %>% 
-  base::split(f = sample(1:n_subbatches, size = nrow(.), replace = TRUE))
-
+# parallelize
 (start <- Sys.time())
-
-future::plan(strategy = "multiprocess", workers = n_subbatches)
-
-furrr::future_map(.x = subbatches, .f = function(this_batch) {
-  
-  out <- 
-    this_batch %>% 
-    slider::slide(.f = ~ .) %>%  # Using slider::slide() as a rowwise iterator
-    purrr::map(.f = function(this_goes) {
-      
-      aws_path <- this_goes$aws_path
-      filename <- stringr::str_sub(this_goes$filename, start = 1, end = -4)
-      scan_center <- this_goes$scan_center
-      local_path <- glue::glue("data/raw/goes16/{scan_center}_{filename}.nc")
-      
-      get_goes_points(aws_path, filename, scan_center, local_path)
-      
-    })
-  
-})
-
+pblapply(X = batches, FUN = get_goes_points)
 (difftime(Sys.time(), start))

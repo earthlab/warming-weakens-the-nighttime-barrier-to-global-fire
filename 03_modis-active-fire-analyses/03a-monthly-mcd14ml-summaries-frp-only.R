32e0f601a3c5553d3a1d56e07d7825105482e32b
‘admahood’ <admahood@gmail.com>
2021-08-20 22:42:13 +0000

    updated

--- 03_modis-active-fire-analyses/03a-monthly-mcd14ml-summaries-frp-only.R
+++ 03_modis-active-fire-analyses/03a-monthly-mcd14ml-summaries-frp-only.R
@@ -70,12 +70,12 @@ monthly_afd_l <-
                                by = .(acq_month, acq_year, dn_detect)]
     
     year_month_afd_lc <- year_afd[, .(q90_frp = q90(frp)), 
-                               by = .(lc, acq_month, acq_year, dn_detect)]%>% 
+                               by = .(str_c("lc_", lc), acq_month, acq_year, dn_detect)]%>% 
       na.omit() %>%
       pivot_wider(names_from = lc, values_from = c(q90_frp))
     
     year_month_afd_k <- year_afd[, .(q90_frp = q90(frp)), 
-                                  by = .(str_sub(lc,1,1), acq_month, acq_year, dn_detect)]%>% 
+                                  by = .(str_c("koppen_",str_sub(lc,1,1)), acq_month, acq_year, dn_detect)]%>% 
       na.omit() %>%
       pivot_wider(names_from = str_sub, values_from = c(q90_frp))
     
@@ -95,3 +95,20 @@ monthly_afd <- data.table::rbindlist(monthly_afd_l, fill = TRUE)
 data.table::fwrite(x = monthly_afd, file = "data/out/mcd14ml_q90-frp_month-lc-kop-daynight-summary.csv")
 
 system2(command = "aws", args = "s3 cp data/out/mcd14ml_q90-frp_month-lc-kop-daynight-summary.csv s3://earthlab-mkoontz/warming-weakens-the-nighttime-barrier-to-global-fire/data/out/")
+
+# basic plots
+
+ggplot(monthly_afd, aes(x=as.Date(paste(acq_year, acq_month, "01", sep="-")), y= q90_frp, color = dn_detect)) +
+  geom_line() +
+  geom_smooth()
+
+monthly_afd %>% 
+  dplyr::select(acq_month, acq_year, dn_detect, `1`,`2`,`3`,`4`) %>%
+  mutate(date = as.Date(paste(acq_year, acq_month, "01", sep="-"))) %>%
+  pivot_longer(cols = c(`1`, `2`,`3`,`4`), names_to = "koppen", values_to = "q90_frp",
+               names_repair = "minimal") %>%
+  ggplot(aes(x=date, y=q90_frp)) +
+  geom_line() +
+  facet_wrap(~koppen) +
+  geom_smooth(method = "lm")
+ 
